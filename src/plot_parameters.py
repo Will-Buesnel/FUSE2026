06052f7fbd6c762d3cc0e3c59a24af0b3b482b01
Will Buesnel <BuesnelWill@gmail.com>
2026-07-22 11:46:51 +0100

    Add figure plot files

---
+++ src/plot_parameters.py
@@ -0,0 +1,73 @@
+"""Will Buesnel, Jul 26."""
+
+from __future__ import annotations
+from utils import get_ax
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from models.parameters import get_all_parameter_interpolants
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def main():
+    processed_dir = Path.cwd().resolve() / "data" / "processed"
+    param_df = pd.read_csv(processed_dir / "MLP001_params.csv")
+    ocv_df = pd.read_csv(processed_dir / "MLP001_ocv.csv")
+
+    # get dict of parameter interpolants.
+    param_interpolants = get_all_parameter_interpolants(param_df, ocv_df)
+
+    socs = np.linspace(0, 1, 100)
+    base_temp = 25 * np.ones_like(socs)
+
+    temps = np.linspace(5, 40, 100)
+    base_soc = 0.5 * np.ones_like(temps)
+
+    fig = plt.figure(constrained_layout=True)
+    gs = fig.add_gridspec(3, 2)
+
+    # define axes for subplots
+    ax_soc_ocv = fig.add_subplot(gs[0, :]) # rectangular plot ie span both columns
+
+    # the rest are square plots, so we can use the same aspect ratio for all of them
+    ax_soc_r = fig.add_subplot(gs[1, 0])
+    ax_soc_tau = fig.add_subplot(gs[2, 0])
+
+    ax_temp_r = fig.add_subplot(gs[1, 1])
+    ax_temp_tau = fig.add_subplot(gs[2, 1])
+
+    r_colnames = ["R0 [Ohm]", "R1 [Ohm]", "R2 [Ohm]"]
+    tau_colnames = ["tau1 [s]", "tau2 [s]"]
+
+
+    # plot values (& evaluate interpolants) for each parameter
+    # ocv: for each temperature, plot OCV vs SoC
+    for temp in param_df["Temperature_degC"].unique():
+        ax_soc_ocv.plot(socs, param_interpolants["OCV[V]"](socs, temp), label=f"T={temp}°C")
+    ax_soc_ocv.legend(frameon=False, bbox_to_anchor=[1, 1, 0.30, 0])
+    ax_soc_ocv.set_title("OCV vs SoC at 25°C")
+    ax_soc_ocv.set_xlabel("State of Charge (SoC)")
+    ax_soc_ocv.set_ylabel("Open Circuit Voltage (V)")
+
+    for r in r_colnames:
+        ax_soc_r.plot(socs, param_interpolants[r](socs, base_temp), label=r)
+        ax_temp_r.plot(temps, param_interpolants[r](base_soc, temps), label=r)
+
+    for tau in tau_colnames:
+        ax_soc_tau.plot(socs, param_interpolants[tau](socs, base_temp), label=tau)
+        ax_temp_tau.plot(temps, param_interpolants[tau](base_soc, temps), label=tau)
+
+    # add legends, outside and to the right of the plots. the resistance and time constant plots share the same legend, so we can add it to the right of the 2nd column of plots
+    ax_temp_r.legend(frameon=False, bbox_to_anchor=[1, 1, 0.60, 0])
+    ax_temp_tau.legend(frameon=False, bbox_to_anchor=[1, 1, 0.60, 0])
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
+
+
--- src/plot_wltp_validation.py
+++ src/plot_wltp_validation.py
@@ -1,6 +1,7 @@
 """Will Buesnel, Jul 26."""
 
 from __future__ import annotations
+from typing import Tuple
 from utils import get_ax
 import pandas as pd
 import numpy as np
@@ -8,7 +9,7 @@ from pathlib import Path
 
 
 def get_drive_cycle_df(cycler_file: str) -> pd.DataFrame:
-    drive_df = pd.read_excel(cycler_file, engine="openpyxl", sheet_name="record")
+    drive_df = pd.read_csv(cycler_file)
 
     # add an hours column to df:
     # data are split into 'hours:seconds:milliseconds.'
@@ -19,16 +20,85 @@ def get_drive_cycle_df(cycler_file: str) -> pd.DataFrame:
     # take away the initial time to get elapsed time.
     hours = hours - hours[0]
 
-    drive_df["Elapsed Time"] = hours
+    drive_df["Elapsed Time[h]"] = hours
 
     return drive_df
 
 
+def get_errors_by_resampling(model_ts, experiment_times, experiment_voltages, model_voltages) -> np.ndarray:
+    # Interpolate the model voltages at the experiment time points
+    resampled_exp_voltages = np.interp(model_ts, experiment_times, experiment_voltages)
+    # Calculate the errors
+    errors = model_voltages - resampled_exp_voltages
+    return errors
+
+
+def run_simulation_from_exp(
+        exp_df: pd.DataFrame,
+        ocv_df: pd.DataFrame,
+        param_file: str,
+        capacity_Ah: float,
+        initial_soc: float,
+        t_max: float | None = None,
+        **kwargs
+) -> dict:
+    """
+    Run a simulation of the electrical model using the given experimental data and parameters.
+    The simulation will use the current data from the experiment and the parameters from the parameter file.
+    The simulation will run for t_max seconds, or until the end of the experimental data if
+    the exp_df will consists of Time, Current and Voltage
+    Returns: res, a dictionary containing simulation results. See code below for more details.
+    The res["t"] will be in seconds, and the res["soc"] will be a fraction between 0 and 1.
+    """
+    # check t_max is not None, if it is then set it to the max time in the experiment data.
+    if t_max is None:
+        t_max = exp_df["Elapsed Time[h]"].max() * 3600  # convert to seconds
+
+    # get various dfs:
+    param_df = pd.read_csv(param_file)
+
+    experiment_times = exp_df["Elapsed Time[h]"].to_numpy() * 3600  # convert to seconds.
+    experiment_currents = exp_df["Current(A)"].to_numpy()
+    experiment_voltages = exp_df["Voltage(V)"].to_numpy()
+
+    # define current funciton that interpolates the current from the experiment data.
+    def current_func(t):
+        return np.interp(t, experiment_times, experiment_currents)
+    
+    # build the model and run the simulation.
+    from models.electrical import ElectricalModel
+    from models.parameters import get_all_parameter_interpolants
+
+    param_interpolants = get_all_parameter_interpolants(param_df, ocv_df) # remember this is a dict of interpolants.
+    # set the interpolants as funcs for the model
+    model = ElectricalModel()
+
+    for name, interpolant in param_interpolants.items():
+        name = name.split(" ")[0].split("[")[0].lower()  # take only the first part of the name, e.g. "R0 [Ohm]" -> "R0"
+        setattr(model, f"_{name}_interp", interpolant) 
+
+    
+    res = model.simulate(
+        y0=[initial_soc, 0, 0],
+        current_func= current_func, 
+        t_max=t_max,
+        max_capacity_Ah=capacity_Ah,
+        **kwargs
+    )
+
+    return res
+
+
 def make_plot(
         exp_df: pd.DataFrame,
+        model_results: Tuple[np.ndarray, np.ndarray, np.ndarray],
         save_name: str | None = None
 ):
-    "Currently unfinished; just has the functionality to plot experiment data."
+    """
+    exp_df: DataFrame containing the experimental data with columns "Elapsed Time[h]", "Voltage(V)", "Current(A)"
+    model_results: Tuple containing the model results (times, voltages, errors) The time for this should also be in hours to keep consistent.
+    save_name: Optional name to save the plot. If None, the plot will be shown
+    """
     (ax_I, ax_v, ax_e), tidy_up = get_ax(
         bool(save_name),
         n_axes=3,
@@ -38,26 +108,93 @@ def make_plot(
 
     ax_I.set_xticks([])
     ax_v.set_xticks([])
-   
-    "plot experiment data"
+    ax_e.set_xticks([])
+
+    # plot the applied current from the experiment
+    ax_I.plot(
+        exp_df["Elapsed Time[h]"],
+        exp_df["Current(A)"],
+        label="Applied Current",
+        color="tab:green",
+    )
+    ax_I.set_ylabel("Current (A)")
+
+    # plot experiment data
     ax_v.plot(
-            exp_df["Elapsed Time"],
+            exp_df["Elapsed Time[h]"],
             exp_df["Voltage(V)"],
             label="Experiment",
             color="tab:cyan",
         )
+    ax_v.set_ylabel("Voltage (V)")
+    # plot model data
+    model_ts, model_vs, model_errors = model_results
+    ax_v.plot(
+            model_ts,
+            model_vs,
+            label="Model",
+            color="tab:orange",
+        )
+    ax_v.legend(frameon=False, loc="lower left")
+    # plot errors
+    ax_e.plot(
+            model_ts,  
+            model_errors,
+            label="Errors",
+            color="tab:brown",
+        )
+    ax_e.set_ylabel("Model error (V)")
+    ax_e.set_xlabel("Time (h)")
 
     tidy_up(save_name)
 
-def main(cycler_file):
+
+def main(cycler_file, ocv_file, param_file):
     exp_df = get_drive_cycle_df(cycler_file)
-    make_plot(exp_df)
+    ocv_df = pd.read_csv(ocv_file)
+
+
+    model_results = run_simulation_from_exp(
+        exp_df=exp_df,
+        ocv_df=ocv_df,
+        param_file=param_file,
+        capacity_Ah=2.2,  # Ah
+        initial_soc=1.0,
+        pbar=True,
+    )
+    v_sim = model_results["v_cell"]
+
+    error = get_errors_by_resampling(
+        model_results["t"],
+        exp_df["Elapsed Time[h]"].to_numpy() * 3600,  # convert to seconds
+        exp_df["Voltage(V)"].to_numpy(),
+        v_sim,
+    )
+    make_plot(exp_df, (model_results["t"] * 1/3600, v_sim, error), save_name=None)
+
+
+    # temporary plot of SoC vs time, for debugging purposes.
+    import matplotlib.pyplot as plt
+    plt.plot(
+        model_results["t"] * 1/3600,
+        model_results["soc"],
+        label="SoC",
+        color="tab:blue",
+    )
+    plt.xlabel("Time (h)")
+    plt.ylabel("SoC")
+    plt.legend(frameon=False)
+    plt.show()
+    print(f"{np.unique(model_results['soc'])}")
 
 if __name__ == "__main__":
     root = Path.cwd().resolve()
     raw_data_dir = root / "data" / "raw"
-    wltp_file = raw_data_dir / "MLP001_wltp_25degC.xlsx"
-    main(cycler_file=wltp_file)
+    processed_data_dir = root / "data" / "processed"
+    wltp_file = processed_data_dir / "MLP001_wltp_25degC_record.csv"
+    param_file = processed_data_dir / "MLP001_params.csv"
+    ocv_file = processed_data_dir / "MLP001_ocv.csv"
+    main(cycler_file=wltp_file, ocv_file=ocv_file, param_file=param_file)
 
     
 
